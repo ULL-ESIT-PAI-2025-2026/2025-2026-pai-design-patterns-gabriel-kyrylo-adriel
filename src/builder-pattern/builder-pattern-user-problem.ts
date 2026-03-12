95fc999cc85aef8b79fb472cc690a3f9900691d6
Gabriel Ruiz Ruiz <alu0101618586@ull.edu.es>
2026-03-12 22:57:51 +0000

    Builder pattern minor fixes

---
+++ src/builder-pattern/builder-pattern-user-problem.ts
@@ -0,0 +1,69 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas 2025-2026
+ *
+ * @author Adriel Reyes Suárez
+ * @author Francisco Gabriel Ruiz Ruiz
+ * @author Kyrylo Chvanov
+ * @since Mar 7 2026
+ * @desc Program that exemplifies the problems that can be solved with the
+ * builder pattern. For academic purposes, all classes involved will be
+ * declared and implemented in this same file.
+ * @see {@link https://refactoring.guru/design-patterns/builder}
+ * @see {@link https://www.captaindebug.com/2011/05/telescoping-constructor-antipattern}
+ */
+
+/**
+ * @desc Class that represents a generic user with a name, DNI, address and city
+ * (both simplified to be represented as a string).
+ */
+export class User {
+  /**
+   * @desc Creates an instance of the object User given a name and optionally a DNI,
+   * address and city (both as strings). Does not thoroughly check if the
+   * input is valid.
+   * @param name First name of the user.
+   * @param dni National Identity Card of the user.
+   * @param address Simplified address of the user.
+   * @param city Name of the city the user lives in. 
+   * @throws {Error} If the name is an empty string.
+   * @remark
+   * Another practice (known as anti pattern) that the builder pattern also
+   * aims to solve is the telescoping constructor (anti)pattern. In a nutshell,
+   * it refers to the practice of overloading a constructor to cover many
+   * different parameter combinations.
+   */
+  constructor(private readonly name: string, private readonly dni?: number,
+      private readonly address?: string, private readonly city?: string) {
+    if (this.name.length === 0) {
+      throw new Error('Tried to create an instance of User with an empty name!');
+    }
+  }
+
+  /**
+   * @desc Returns a string that is a representation of the user.
+   * @return String that details all the known data about the user.
+   */
+  toString(): string {
+    return `User(name: ${this.name}, dni: ${this.dni || 'N/A'}, ` +
+        `address: ${this.address || 'N/A'}, city: ${this.city || 'N/A'})`;
+  }
+}
+
+/**
+ * @desc Main entry point for the program that exemplifies what the builder
+ * pattern aims to solve.
+ */
+export function main() {
+  // We know the name and dni - constructor seems fine.
+  const jorge = new User('Jorge', 12345678);
+  console.log(jorge.toString());
+  // We only know the name and city...
+  const abel = new User('Abel', 0, '', 'Sta. Cruz');
+  console.log(abel.toString());
+  // Conclusion: this constructor is not comfortable to work with!
+}
+
+main();
